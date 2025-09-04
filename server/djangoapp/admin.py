2d7186c8bab82069bfac4606fe480931c3b4b0d5
HappyCreators <bestself.happysoul@gmail.com>
2025-09-03 20:10:42 -0600

    Update admin.py
    PEP8

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1  # How many empty forms to display
 
+
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
+
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'type', 'year')
+
 
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
