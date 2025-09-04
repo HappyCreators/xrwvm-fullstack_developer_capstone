3dcf57d13c478da19bcf7b8ce2d35c11e4e8cd4b
HappyCreators <bestself.happysoul@gmail.com>
2025-09-04 15:58:30 -0400

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-echo "Making migrations and migrating the database."
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-
-# Create superuser if it doesn't exist
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='root').exists(): \
-    User.objects.create_superuser('root', 'admin@mail.com', 'root')" | python manage.py shell
-
 exec "$@"
